049f7df2d180cf3337f65b02bdeea48be5cf6949
Andy <159312225+Mw4mba@users.noreply.github.com>
2025-05-04 00:16:13 +0200

    python update

--- main.py
+++ main.py
@@ -8,18 +8,21 @@ page_html=requests.get('https://www.ourpower.co.za/areas/jhb-city-power').text
 formatted=BeautifulSoup(page_html,'html.parser')
 links=formatted.findAll('tr')
 
-newJson=[{}]
+newJson=[]
 
 #for link in formatted.find_all('a'):
  #   print(link.get('href'))
 
 for item in formatted.find_all('tr'):
-    newJson.append(
+    if item.contents[0].text != "Time" and len(item.contents[1].text)>1:
+            newJson.append(
         {
         "TIME":item.contents[0].text,
         "AREAS":item.contents[1].text.split(",")
         }
         )
+
+
 
 
 print(newJson)  
