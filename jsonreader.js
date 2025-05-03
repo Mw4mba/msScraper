93d5ec8a36a3cfb8c3f229fb61352e2e7b4ec626
Andy <159312225+Mw4mba@users.noreply.github.com>
2025-05-04 00:49:38 +0200

    comments additoin

--- jsonreader.js
+++ jsonreader.js
@@ -1,12 +1,14 @@
-const jsonSource= "./sample.json";
+const jsonSource= "sample.json";
 //const myJson=await fetch('sample.json')
 
+//this converts the json into an array type object that is useful for operations
 const getrawData = async () => {
     const res = await fetch(jsonSource); //works on server but not on local file
     const data = await  res.json();
     return data
 }
 
+//this is mean to store the array but the source of the json is causing problems 
 const myArray=getrawData()
 const myData=myArray[0]
 
